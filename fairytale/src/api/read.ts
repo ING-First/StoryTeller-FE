48517b6df8363b8be8b736dd04f9ef1a00af3516
KIMSS <kcg199510@gmail.com>
2025-08-20 14:18:15 +0900

    Fix: 버그 수정

--- fairytale/src/api/read.ts
+++ fairytale/src/api/read.ts
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-// 환경변수 기반 엔드포인트 호스팅 주소 반영
 const API_BASE = process.env.REACT_APP_API_BASE
 
-// 동화책 페이지 읽기
 export async function readFairyTalePage(
   uid: number,
   fid: number,
@@ -14,7 +12,6 @@ export async function readFairyTalePage(
   const tokenType = localStorage.getItem('token_type') || 'bearer'
 
   try {
-    // 로그인된 경우에만 동화책 읽기 가능
     const res = await axios.post(
       `${API_BASE}/users/${uid}/fairy_tales/${fid}/read`,
       {page, voice_id},
@@ -22,7 +19,8 @@ export async function readFairyTalePage(
         headers: {
           Authorization: `${tokenType} ${token}`,
           'Content-Type': 'application/json'
-        }
+        },
+        responseType: 'blob'
       }
     )
     return res.data
