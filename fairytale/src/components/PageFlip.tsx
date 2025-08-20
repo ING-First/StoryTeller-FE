5102af2f2c35e86b4affd0212bfacacde7c5a619
KIMSS <kcg199510@gmail.com>
2025-08-20 14:15:40 +0900

    Feat: 동화책 읽기 페이지 구현

--- fairytale/src/components/PageFlip.tsx
+++ fairytale/src/components/PageFlip.tsx
@@ -1,43 +1,52 @@
 // src/components/PageFlip.tsx
-import React from 'react'
+import React, {forwardRef} from 'react'
 import HTMLFlipBook from 'react-pageflip'
 
 interface PageFlipProps {
   width?: number
   height?: number
   children?: React.ReactNode
+  onFlip?: (e: any) => void
 }
 
-const PageFlip: React.FC<PageFlipProps> = ({width = 800, height = 1000, children}) => {
-  return (
-    <HTMLFlipBook
-      width={width}
-      height={height}
-      className="book"
-      size="fixed"
-      minWidth={600}
-      maxWidth={800}
-      minHeight={800}
-      maxHeight={1000}
-      drawShadow={true}
-      flippingTime={1000}
-      usePortrait={true}
-      startZIndex={0}
-      autoSize={true}
-      maxShadowOpacity={1}
-      showCover={false}
-      mobileScrollSupport={true}
-      swipeDistance={30}
-      clickEventForward={true}
-      useMouseEvents={true}
-      renderOnlyPageLengthChange={false}
-      startPage={0}
-      showPageCorners={true}
-      disableFlipByClick={false}
-      style={{}}>
-      {children}
-    </HTMLFlipBook>
-  )
-}
+const PageFlip = forwardRef<any, PageFlipProps>(
+  ({width = 800, height = 1000, children, onFlip}, ref) => {
+    const FlipBookComponent = HTMLFlipBook as any
+
+    return (
+      <FlipBookComponent
+        ref={ref}
+        width={width}
+        height={height}
+        className="book"
+        size="fixed"
+        minWidth={600}
+        maxWidth={800}
+        minHeight={800}
+        maxHeight={1000}
+        drawShadow={true}
+        flippingTime={1000}
+        usePortrait={true}
+        startZIndex={0}
+        autoSize={true}
+        maxShadowOpacity={1}
+        showCover={false}
+        mobileScrollSupport={true}
+        swipeDistance={30}
+        clickEventForward={true}
+        useMouseEvents={true}
+        renderOnlyPageLengthChange={false}
+        startPage={0}
+        showPageCorners={true}
+        disableFlipByClick={false}
+        onFlip={onFlip}
+        style={{}}>
+        {children}
+      </FlipBookComponent>
+    )
+  }
+)
+
+PageFlip.displayName = 'PageFlip'
 
 export default PageFlip
